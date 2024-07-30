1bd2fdc9a70b647b04f83c116415df7b1b3ddff1
daniloas1992 <danilo_as1992@hotmail.com>
2024-07-29 22:14:49 -0300

    Update Lemonade App

--- Lemonade/app/src/main/java/com/danilo/alves/lemonade/MainActivity.kt
+++ Lemonade/app/src/main/java/com/danilo/alves/lemonade/MainActivity.kt
@@ -3,15 +3,19 @@ package com.danilo.alves.lemonade
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -20,12 +24,15 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.danilo.alves.lemonade.ui.theme.LemonadeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -51,6 +58,8 @@ fun Lemonade(modifier: Modifier = Modifier) {
 fun LemonadeTree(modifier: Modifier) {
 
     var result by remember { mutableStateOf(1) }
+    var taps by remember { mutableStateOf(1) }
+    var counter by remember { mutableStateOf(100) }
 
     val imageResource = when (result) {
         1 -> R.drawable.lemon_tree
@@ -76,8 +85,33 @@ fun LemonadeTree(modifier: Modifier) {
     Column (modifier = modifier,
             horizontalAlignment = Alignment.CenterHorizontally)
     {
-        Image(painter = painterResource(id = imageResource), contentDescription = description.toString())
+        Image(painter = painterResource(id = imageResource),
+              contentDescription = description.toString(),
+              contentScale = ContentScale.Crop,
+              modifier = Modifier.clip(RoundedCornerShape(30.dp))
+                                 .background(Color(0xFFCBEBD4))
+                                 .padding(30.dp)
+                                 .clickable {
+                                    if(result == 2 && counter > 0) {
+                                        if(counter == 100) {
+                                            counter = (3..7).random()
+                                            taps = counter
+                                        } else {
+                                            taps--
+                                        }
+                                        if(taps == 0) {
+                                            counter = 100
+                                            ++result
+                                        }
+                                    } else if (result < 4) {
+                                        ++result
+                                    } else {
+                                        result = 1
+                                    }
+                                 }
+        )
+
         Spacer(modifier = Modifier.height(30.dp))
-        Text(text = stringResource(instruction), fontSize =  18.sp)
+        Text(text = stringResource(instruction), fontSize =  18.sp, style = MaterialTheme.typography.bodyLarge)
     }
 }
